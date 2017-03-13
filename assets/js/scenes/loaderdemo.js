841e200f7b97ef3eb033459135036ebeb3030646
gl2748 <imaitland@gmail.com>
2017-03-12 22:51:12 -0400

    update loader demo

--- assets/js/scenes/loaderdemo.js
+++ assets/js/scenes/loaderdemo.js
@@ -4,16 +4,19 @@ var camera, scene, renderer;
 
 var geometry, root, root2, objectToggleAddRemove;
 
-//Origin is (0,0)
-//Radius of our  circle is 1000
 var originX = 0;
 var originY = 0;
 var originZ = 0;
-var offsetY = - 0.1;
-var radius = 2;
-var count = 0;
-var colorCount = 0;
+var offsetY = - 0.5;
+
+var radius = 7;
+var width = 1;
+var wavelength = 0.03;
 var length = 400;
+
+var colorCount = 0;
+var count = 0;
+var clickCounter = 0;
 
 var trails = [];
 var trails2 = [];
@@ -22,14 +25,12 @@ var trail, trail2;
 var colors = [ 0x289FC7, 0x51DB56, 0xE647A9];
 var trailColor1 = 0xff0040;
 var trailColor2 = 0x355FDE;
-var clickCounter = 0;
 var clickColor = 0xEBA35B;
 
 var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 document.addEventListener( 'touchstart', onDocumentTouchStart, false );
 
@@ -38,107 +39,102 @@ animate();
 
 function init() {
 
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 15000 );
-  camera.position.z = 50;
-  camera.position.y = -17;
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 15000 );
+	camera.position.z = 50;
+	camera.position.y = -17;
 
-  scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
-  var geometry = new THREE.SphereGeometry( 2, 16, 16 );
-  var material = new THREE.MeshBasicMaterial( { color: 0xff0040 } )
+	var geometry = new THREE.SphereGeometry( width, 16, 160 );
+	var material = new THREE.MeshBasicMaterial( { color: 0xff0040 } )
 
-  for ( var i = 0; i < length; i ++ ) {
-    trail = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: trailColor1 } ) );
-    trail.position.y = i * offsetY;
-    trails[i] = trail;
-  };
+	for ( var i = 0; i < length; i ++ ) {
+		trail = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: trailColor1 } ) );
+		trail.position.y = i * offsetY;
+		trails[i] = trail;
+	};
 
-  for ( var i = 0; i < length; i ++ ) {
-    trail2 = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: trailColor2 } ) );
-    trail2.position.y = i * offsetY;
-    trails2[i] = trail2;
-  };
+	for ( var i = 0; i < length; i ++ ) {
+		trail2 = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: trailColor2 } ) );
+		trail2.position.y = i * offsetY;
+		trails2[i] = trail2;
+	};
 
-  trails.forEach(function(trail){
-    scene.add( trail );
-  });
+	trails.forEach(function(trail){
+		scene.add( trail );
+	});
 
-  trails2.forEach(function(trail){
-    scene.add( trail );
-  });
+	trails2.forEach(function(trail){
+		scene.add( trail );
+	});
 
-  renderer = new THREE.WebGLRenderer({});
-  renderer.setClearColor( 0x000000 );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.sortObjects = false;
-  container.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer({});
+	renderer.setClearColor( 0x000000 );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.sortObjects = false;
+	container.appendChild( renderer.domElement );
 
-  window.addEventListener( 'resize', onWindowResize, false );
+	window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function onDocumentMouseMove(event) {
-  mouseX = ( event.clientX - windowHalfX ) * 10;
-  mouseY = ( event.clientY - windowHalfY ) * 10;
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 function onDocumentMouseDown(event) {
-  colorCount = 0;
-  clickColor = colors[clickCounter];
-  clickColor2 = colors[clickCounter - 1];
-  clickCounter += 1;
-  if (clickCounter > 2) {
-    clickCounter = 0;
-  }
+	colorCount = 0;
+	clickColor = colors[clickCounter];
+	clickColor2 = colors[clickCounter - 1];
+	clickCounter += 1;
+	if (clickCounter > 2) {
+		clickCounter = 0;
+	}
 }
 
 function onDocumentTouchStart( event ) {
-  colorCount = 0;
-  clickColor = colors[clickCounter];
-  clickColor2 = colors[clickCounter - 1];
-  clickCounter += 1;
-  if (clickCounter > 2) {
-    clickCounter = 0;
-  }
+	colorCount = 0;
+	clickColor = colors[clickCounter];
+	clickColor2 = colors[clickCounter - 1];
+	clickCounter += 1;
+	if (clickCounter > 2) {
+		clickCounter = 0;
+	}
 }
 
 function animate() {
-  requestAnimationFrame( animate );
-  render();
+	requestAnimationFrame( animate );
+	render();
 }
 
 function render() {
 
-  for ( var i = 0; i < trails.length; i ++ ) {
-    var nudge = i * 0.03;
-    trails[ i ].position.x = originX + radius * Math.cos(nudge + count*0.03);
-    trails[ i ].position.z = originZ + radius * Math.sin(nudge + count*0.03);
-  }
+	for ( var i = 0; i < trails.length; i ++ ) {
+		var nudge = i * wavelength;
+		trails[ i ].position.x = originX + radius * Math.cos(nudge + count*0.03);
+		trails[ i ].position.z = originZ + radius * Math.sin(nudge + count*0.03);
+	}
 
-  trails[ colorCount ].material.color.setHex(clickColor);
+	trails[ colorCount ].material.color.setHex(clickColor);
 
-  for ( var i = 0; i < trails2.length; i ++ ) {
-    var nudge = i * 0.03;
-    trails2[ i ].position.x = originX + radius * Math.cos(Math.PI + nudge + count*0.03);
-    trails2[ i ].position.z = originZ + radius * Math.sin(Math.PI + nudge + count*0.03);
-  }
+	for ( var i = 0; i < trails2.length; i ++ ) {
+		var nudge = i * wavelength;
+		trails2[ i ].position.x = originX + radius * Math.cos(Math.PI + nudge + count*0.03);
+		trails2[ i ].position.z = originZ + radius * Math.sin(Math.PI + nudge + count*0.03);
+	}
 
-  count += 1;
-  colorCount += 1;
+	count += 1;
+	colorCount += 1;
 
-  if (colorCount > length -1 ) {
-    colorCount = 0;
-  }
-  renderer.render( scene, camera );
+	if (colorCount > length -1 ) {
+		colorCount = 0;
+	}
+	renderer.render( scene, camera );
 }
